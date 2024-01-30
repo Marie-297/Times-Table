110ef3fd5206e5660b5da3dff903c30fe0617d9e
Mariama Seidu <merrylyt@gmail.com>
2024-01-30 12:18:27 +0000

    Multiplication Times table using Javascript

---
+++ timestable.js
@@ -0,0 +1,13 @@
+function onClickDownKey(event) {
+  if (event.key === "Enter") {
+    timesTable();
+  };
+};
+
+function timesTable() {
+const numberSelect =  document.querySelector('.choosenum').value;
+
+for (let i = 1; i <= 12; i++) {
+  document.querySelector('.tble').innerHTML += `${numberSelect} * ${i} = ${numberSelect * i}<br><br>`;
+  };
+};
